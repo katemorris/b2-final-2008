d2f28fe611022479f44b121e675479ec1b83aacc
Kate Tester <katemorris@gmail.com>
2020-11-09 10:31:11 -0700

    Completes hospital show page with passing tests on all methods.

--- app/models/hospital.rb
+++ app/models/hospital.rb
@@ -1,3 +1,11 @@
 class Hospital < ApplicationRecord
   has_many :doctors
+
+  def num_doctors
+    doctors.count
+  end
+
+  def unique_universities
+    doctors.pluck(:university).uniq
+  end
 end
--- spec/models/hospital_spec.rb
+++ spec/models/hospital_spec.rb
@@ -4,4 +4,42 @@ RSpec.describe Hospital, type: :model do
   describe "relationships" do
     it { should have_many :doctors }
   end
+
+  describe "instance methods" do
+    before(:each) do
+      @seattle = Hospital.create!(name: "Seattle Grace")
+      @mcdreamy = @seattle.doctors.create!(
+        name: "Derek 'McDreamy' Shepherd",
+        speciality: 'Attending Surgeon',
+        university: 'University of Pennsylvania'
+      )
+      @bailey = @seattle.doctors.create!(
+        name: "Miranda Bailey",
+        speciality: 'General Surgery',
+        university: 'Stanford University'
+      )
+      @meredith = @seattle.doctors.create!(
+        name: "Meredith Grey",
+        speciality: 'General Surgery',
+        university: 'Harvard University'
+      )
+      @susan = @seattle.doctors.create!(
+        name: "Susan Bones",
+        speciality: 'General Surgery',
+        university: 'Harvard University'
+      )
+      @seaside = Hospital.create!(name: "Seaside Health & Wellness Center")
+    end
+
+    it '#num_doctors' do
+      expect(@seattle.num_doctors).to eq(4)
+      expect(@seaside.num_doctors).to eq(0)
+    end
+
+    it '#unique_universities' do
+      expected = ['University of Pennsylvania', 'Stanford University', 'Harvard University']
+      expect(@seattle.unique_universities.sort).to eq(expected.sort)
+      expect(@seaside.unique_universities.sort).to eq([])
+    end
+  end
 end
